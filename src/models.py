29d12cbf5672628c70cfc6d3cd72dba4c2e88f4f
velazcopolo <144776724+velazcopolo@users.noreply.github.com>
2024-01-29 19:50:08 +0000

    starwars sql data

--- src/models.py
+++ src/models.py
@@ -7,26 +7,66 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    pwd = Column(Integer, nullable=False)
+    email = Column(String(250), nullable=False)
+    
+    
+
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    height = Column(Integer, nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    person_id = Column(Integer, ForeignKey('planet.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    length = Column(Integer, nullable=False)
+    manufacturer = Column(Integer, nullable=False )
     person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    # person = relationship(Person)
+
+  
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    diameter = Column(Integer, nullable=False)
+    climate = Column(String(250), nullable=False)
+    population = Column(Integer, nullable=False)
+    # person = relationship(Person)
+
+   
+
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
 
     def to_dict(self):
         return {}
-
+    
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
